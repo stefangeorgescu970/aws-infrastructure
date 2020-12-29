04a6e4310ae50a78974938b0da0b5dba1265fdcb
Stefan Georgescu <stefan.georgescu.970@gmail.com>
2020-12-29 20:40:59 +0200

    add bucket for backups

--- modules/plc_webapp/main.tf
+++ modules/plc_webapp/main.tf
@@ -2,3 +2,8 @@ module "backups_bucket" {
   source      = "../../stacks/s3/bucket"
   bucket_name = "plc-metrics-webapp-db-backup-${var.stage}"
 }
+
+module "logs_bucket" {
+  source      = "../../stacks/s3/bucket"
+  bucket_name = "plc-metrics-webapp-logs-${var.stage}"
+}
